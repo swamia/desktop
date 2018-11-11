8da746dc19d68a382f2cf63e61b6b11e8f51de27
avvaru <swami@gmail.com>
2018-11-11 19:55:27 +0530

    job

---
+++ src/main/java/maruthi/App.java
@@ -0,0 +1,13 @@
+package maruthi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
